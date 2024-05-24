25117c9755b58cdc7ef7b6142a848639e7f60406
Mahdi <mahdimeyghani02@gmail.com>
2024-05-24 17:28:45 +0330

    Add fake dates and temperatures data

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ days = st.slider(label="Forecast Days", min_value=1, max_value=5,
 option = st.selectbox(label="Select data to view", options=("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} in {place}")
+
+
+def get_data(day):
+    dates = ["2024-24-10", "2024-25-10", "2024-26-10"]
+    temperatures = [10, 11, 20]
+
+    temperatures = [i * day for i in temperatures]
+
+    return dates, temperatures
+
+
+d, t = get_data(days)
